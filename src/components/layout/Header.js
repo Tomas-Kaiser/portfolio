1feb42c731f11853d8bbfe76a97fc54f972f13ba
Tomas Kaiser <tomas.kaiser06@gmail.com>
2019-08-10 21:55:32 +0100

    Added Header component

---
+++ src/components/layout/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+   return(
+      <header>
+         <h1>Tomas Kaiser</h1>
+         <p>I am a full stack software developer</p>
+      </header>
+   )
+}
+
+export default Header;
