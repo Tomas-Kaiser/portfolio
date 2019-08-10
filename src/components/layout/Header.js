3b26ace92d505c8878fbdc72fce3a2699c8a969e
Tomas Kaiser <tomas.kaiser06@gmail.com>
2019-08-10 22:40:14 +0100

    Updated Header with new bg img, layout & style

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 function Header() {
-   return(
-      <header className="container-fluid">
-         <h1 className="text-center">Tomas Kaiser</h1>
-         <p className="text-center">I am a full stack software developer</p>
+   return (
+      <header className="container-fluid callout">
+         <div className="jumbotron">
+            <h1 className="text-center">Tomas Kaiser</h1>
+            <p className="text-center">Full stack software developer</p>
+         </div>
       </header>
    )
 }
