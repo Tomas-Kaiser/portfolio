9f24fedae63d97e28d24d837207a918c8bb4e0c7
Tomas Kaiser <tomas.kaiser06@gmail.com>
2019-08-10 22:59:53 +0100

    Added Navbar component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from './components/layout/Header';
+import Navbar from './components/layout/Navbar';
 
 import './App.css';
 
@@ -8,6 +9,7 @@ class App extends Component {
     return(
       <div className="App">
         <Header />
+        <Navbar />
       </div>
     )
   }
