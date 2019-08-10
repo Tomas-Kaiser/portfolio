2c9db226d2d7bb1694b8a03d5c2225411fabecee
Tomas Kaiser <tomas.kaiser06@gmail.com>
2019-08-10 22:02:51 +0100

    Put texts to center

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 function Header() {
    return(
-      <header className="container">
-         <h1>Tomas Kaiser</h1>
-         <p>I am a full stack software developer</p>
+      <header className="container-fluid">
+         <h1 className="text-center">Tomas Kaiser</h1>
+         <p className="text-center">I am a full stack software developer</p>
       </header>
    )
 }
