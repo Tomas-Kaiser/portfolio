f208a12e406aae0a2981d8b391ad627a0adba2d4
Tomas Kaiser <tomas.kaiser06@gmail.com>
2019-08-10 21:58:27 +0100

    Added Bootstrap 4

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Header() {
    return(
-      <header>
+      <header className="container">
          <h1>Tomas Kaiser</h1>
          <p>I am a full stack software developer</p>
       </header>
