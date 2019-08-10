4968115985c56c57d80eaeeff05327df92b733c1
Tomas Kaiser <tomas.kaiser06@gmail.com>
2019-08-10 21:39:19 +0100

    Used class instead of function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Hi
-    </div>
-  );
+class App extends Component {
+  render() {
+    return(
+      <div className="App">
+        Hi There
+      </div>
+    )
+  }
 }
 
 export default App;
