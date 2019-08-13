0bc100070318715876338164203156374e91103d
Tomas Kaiser <tomas.kaiser06@gmail.com>
2019-08-13 18:09:12 +0100

    Added data about technologies which I know

---
+++ src/static/data/data.js
@@ -0,0 +1,244 @@
+const languages = [
+   {
+      id: 1,
+      title: "HTML5",
+      img: "HTML.png",
+      exp: "2 years",
+   },
+   {
+      id: 2,
+      title: "CSS3",
+      img: "CSS3.png",
+      exp: "2 years",
+   },
+   {
+      id: 3,
+      title: "JavaScript",
+      img: "Javascript.png",
+      exp: "2 years",
+   },
+   {
+      id: 4,
+      title: "Python",
+      img: "Python.png",
+      exp: "1 year",
+   },
+   {
+      id: 5,
+      title: "SQL",
+      img: "SQL.png",
+      exp: "1 year",
+   },
+   {
+      id: 20,
+      title: "C",
+      img: "C.png",
+      exp: "Basic",
+   },
+];
+
+const databases = [
+   {
+      id: 6,
+      title: "SQLite",
+      img: "SQLite.png",
+      exp: "1 year",
+   },
+   {
+      id: 7,
+      title: "PostgreSQL",
+      img: "PostgreSQL.png",
+      exp: "1 year",
+   },
+   {
+      id: 8,
+      title: "MongoDB",
+      img: "MongoDB.png",
+      exp: "1 year",
+   },
+   {
+      id: 9,
+      title: "mySQL",
+      img: "mySQL.png",
+      exp: "1 year",
+   },
+];
+
+const dataVisualization = [
+   {
+      id: 10,
+      title: "D3.js",
+      img: "D3.png",
+      exp: "1 year",
+   },
+   {
+      id: 11,
+      title: "DC.js",
+      img: "DC.png",
+      exp: "1 year",
+   },
+   {
+      id: 12,
+      title: "Crossfilter.js",
+      img: "Crossfilter.png",
+      exp: "1 year",
+   },
+]
+
+const frameworks = [
+   {
+      id: 13,
+      title: "Django",
+      img: "Django.png",
+      exp: "1 year",
+   },
+   {
+      id: 14,
+      title: "Flask",
+      img: "Flask.png",
+      exp: "1 year",
+   },
+   {
+      id: 15,
+      title: "ReactJS",
+      img: "ReactJS.png",
+      exp: "1 year",
+   },
+   {
+      id: 16,
+      title: "jQuery",
+      img: "jQuery.png",
+      exp: "2 year",
+   },
+   {
+      id: 17,
+      title: "Bootstrap",
+      img: "Bootstrap.png",
+      exp: "2 year",
+   },
+   {
+      id: 38,
+      title: "Materialize",
+      img: "Materialize.png",
+      exp: "2 year",
+   },
+   {
+      id: 18,
+      title: "Jinja2",
+      img: "Jinja2.jpg",
+      exp: "2 year",
+   },
+]
+
+const others = [
+   {
+      id: 19,
+      title: "GIT",
+      img: "GIT.png",
+      exp: "2 year",
+   },
+   {
+      id: 21,
+      title: "GitHub",
+      img: "GitHub.svg",
+      exp: "2 year",
+   },
+   {
+      id: 22,
+      title: "Heroku",
+      img: "Heroku.png",
+      exp: "2 year",
+   },
+   {
+      id: 23,
+      title: "AWS S3",
+      img: "AWS_S3.jpg",
+      exp: "2 year",
+   },
+   {
+      id: 24,
+      title: "Stripe",
+      img: "Stripe.png",
+      exp: "2 year",
+   },
+   {
+      id: 25,
+      title: "JSON",
+      img: "JSON.png",
+      exp: "2 year",
+   },
+   {
+      id: 26,
+      title: "MARKDOWN",
+      img: "MARKDOWN.png",
+      exp: "2 year",
+   },
+   {
+      id: 27,
+      title: "VS Code",
+      img: "VS_Code.png",
+      exp: "2 year",
+   },
+   {
+      id: 28,
+      title: "BASH",
+      img: "BASH.png",
+      exp: "2 year",
+   },
+   {
+      id: 29,
+      title: "Linux - Ubuntu",
+      img: "Ubuntu.jpg",
+      exp: "2 year",
+   },
+   {
+      id: 30,
+      title: "Balsamiq",
+      img: "Balsamiq.png",
+      exp: "2 year",
+   },
+   {
+      id: 31,
+      title: "AdobeXD",
+      img: "AdobeXD.png",
+      exp: "2 year",
+   },
+   {
+      id: 32,
+      title: "Slack",
+      img: "Slack.png",
+      exp: "2 year",
+   },
+   {
+      id: 33,
+      title: "Trello",
+      img: "Trello.png",
+      exp: "2 year",
+   },
+   {
+      id: 34,
+      title: "Microsoft Office",
+      img: "MOffice.png",
+      exp: "10 years",
+   },
+   {
+      id: 35,
+      title: "SAP",
+      img: "SAP.png",
+      exp: "10 years",
+   },
+   {
+      id: 36,
+      title: "SalesForce",
+      img: "SalesForce.png",
+      exp: "10 years",
+   },
+   {
+      id: 37,
+      title: "Gulp",
+      img: "Gulp.png",
+      exp: "10 years",
+   }
+]
+
+export default {languages, databases, dataVisualization, frameworks, others};
