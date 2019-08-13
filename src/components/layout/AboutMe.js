3b57a44eb4de29d1ede3b1d3ebfe7629a04508de
Tomas Kaiser <tomas.kaiser06@gmail.com>
2019-08-13 19:19:25 +0100

    Changed class component to fnc

--- src/components/layout/AboutMe.js
+++ src/components/layout/AboutMe.js
@@ -1,22 +1,14 @@
-import React, { Component } from 'react';
-import data from '../../static/data/data'
+import React from 'react';
 
-class AboutMe extends Component {
-   state = {
-      data
-   }
-
-   render() {
-      console.log(data)
-      return (
-         <section className="container mt-3">
-            <article className="about-me">
-               <h2>About Me</h2>
-               <p>I am an experienced full-stack software developer, holding a diploma in software development, credit-rated by Edinburgh Napier University. I have experience with front-end (HTML5, CSS3, JS) and back-end (Python, SQL, noSQL) technologies, so as 5 years of work experience in B2B and B2C markets.</p>
-            </article>
-          </section>
-      )
-   }
+function AboutMe() {
+   return (
+      <section className="container mt-3">
+         <article className="about-me">
+            <h2>About Me</h2>
+            <p>I am an experienced full-stack software developer, holding a diploma in software development, credit-rated by Edinburgh Napier University. I have experience with front-end (HTML5, CSS3, JS) and back-end (Python, SQL, noSQL) technologies, so as 5 years of work experience in B2B and B2C markets.</p>
+         </article>
+      </section>
+   )
 }
 
 export default AboutMe;
