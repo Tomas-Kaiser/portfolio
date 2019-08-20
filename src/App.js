0dc741135532216fc8fb4be33a49f4a479b54dfe
Tomas Kaiser <tomas.kaiser06@gmail.com>
2019-08-20 22:36:20 +0100

    Added Experience component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/layout/Navbar';
 import AboutMe from './components/layout/AboutMe';
 import Technologies from './components/layout/Technologies';
 import Projects from './components/layout/Projects';
+import Experience from './components/layout/Experience';
 import Contact from './components/layout/Contact';
 import Footer from './components/layout/Footer';
 
@@ -18,6 +19,7 @@ class App extends Component {
         <AboutMe />
         <Technologies />
         <Projects />
+        <Experience />
         <Contact />
         <Footer />
       </div>
---
+++ src/components/layout/Experience.js
@@ -0,0 +1,92 @@
+import React from 'react';
+
+function Experience() {
+   return (
+      <section class="container">
+         <div class="timeline">
+            <ul>
+               <li>
+                  <div class="content">
+                     <h3>Code Institute</h3>
+                     <p>
+                        Diploma in full stack software development, credit-rated by <b>Edinburgh Napier University</b>
+                     </p>
+                  </div>
+                  <div class="time">
+                     <h4>October 2018</h4>
+                  </div>
+               </li>
+               <li>
+                  <div class="content">
+                     <h3>UDACITY, USA</h3>
+                     <p>
+                        Certificate Front End Web Developer
+                     </p>
+                  </div>
+                  <div class="time">
+                     <h4>December 2017</h4>
+                  </div>
+               </li>
+               <li>
+                  <div class="content">
+                     <h3>Forcepoint</h3>
+                     <p>
+                        I currently work as a Hardware Analyst
+                     </p>
+                  </div>
+                  <div class="time">
+                     <h4>November 2017</h4>
+                  </div>
+               </li>
+               <li>
+                  <div class="content">
+                     <h3>Toytoa Tsusho</h3>
+                     <p>
+                        I worked as a customer service specialist
+                     </p>
+                  </div>
+                  <div class="time">
+                     <h4>August 2016</h4>
+                  </div>
+               </li>
+               <li>
+                  <div class="content">
+                     <h3>GEFCO Forwarding</h3>
+                     <p>
+                        I worked as an Indoor Sales Specialist
+                     </p>
+                  </div>
+                  <div class="time">
+                     <h4>November 2014</h4>
+                  </div>
+               </li>
+               <li>
+                  <div class="content">
+                     <h3>University of Pardubice</h3>
+                     <p>
+                        MSc. Technology and Supply Chain Management
+                     </p>
+                  </div>
+                  <div class="time">
+                     <h4>September 2014</h4>
+                  </div>
+               </li>
+               <li>
+                  <div class="content">
+                     <h3>University of Pardubice</h3>
+                     <p>
+                        BSc. Technology
+                     </p>
+                  </div>
+                  <div class="time">
+                     <h4>September 2012</h4>
+                  </div>
+               </li>
+               <div style={{clear: "both"}}></div>
+            </ul>
+         </div>
+      </section>
+   )
+}
+
+export default Experience;
