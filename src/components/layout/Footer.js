dc40fe1dd130a403a3c76985fde51b3098932b58
Tomas Kaiser <tomas.kaiser06@gmail.com>
2019-08-24 22:06:03 +0100

    Amended class to className

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -5,24 +5,24 @@ import { faDownload } from '@fortawesome/free-solid-svg-icons';
 
 function Footer() {
    return (
-      <footer class="container-fluid">
-         <div id="footer-details" class="row">
-            <div class="col-sm-4">
+      <footer className="container-fluid">
+         <div id="footer-details" className="row">
+            <div className="col-sm-4">
                <h5>About</h5>
-               <p class="inline-block">
+               <p className="inline-block">
                   Full Stack Web Development. Skilled in everything from HTML to Heroku.
                   The capacity for structural and design thinking.
             </p>
             </div>
-            <div class="col-sm-4">
+            <div className="col-sm-4">
                <h5>Download CV</h5>
-               <p class="inline-block">
+               <p className="inline-block">
                   Need a printable version of my CV? Send me a message.
                </p>
             </div>
-            <div class="col-sm-4 text-center">
+            <div className="col-sm-4 text-center">
                <h5>MySocial</h5>
-               <ul class="list-inline social-links">
+               <ul className="list-inline social-links">
                   <li>
                      <a href="https://github.com/Tomas-Kaiser" target="_blank"><span><FontAwesomeIcon icon={faGithub} /></span></a>
                      <a href="https://www.linkedin.com/in/tomas-kaiser-06/" target="_blank"><span><FontAwesomeIcon icon={faLinkedin} /></span></a>
