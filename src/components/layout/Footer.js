ef3cddd21f48cd15ad5f8a24ef07ec8640ed7163
Tomas Kaiser <tomas.kaiser06@gmail.com>
2019-08-28 23:12:52 +0100

    Changed content

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -6,18 +6,11 @@ import { faDownload } from '@fortawesome/free-solid-svg-icons';
 function Footer() {
    return (
       <footer className="container-fluid">
-         <div id="footer-details" className="row">
+         <div id="footer-details" className="row justify-content-between">
             <div className="col-sm-4">
-               <h5>About</h5>
+               <h5>About Me</h5>
                <p className="inline-block">
-                  Full Stack Web Development. Skilled in everything from HTML to Heroku.
-                  The capacity for structural and design thinking.
-            </p>
-            </div>
-            <div className="col-sm-4">
-               <h5>Download CV</h5>
-               <p className="inline-block">
-                  Need a printable version of my CV? Send me a message.
+                  I am a Full Stack Web Developer. If you want me hire or work on a project, do not hesitate to contact me
                </p>
             </div>
             <div className="col-sm-4 text-center">
@@ -26,7 +19,7 @@ function Footer() {
                   <li>
                      <a href="https://github.com/Tomas-Kaiser" target="_blank"><span><FontAwesomeIcon icon={faGithub} /></span></a>
                      <a href="https://www.linkedin.com/in/tomas-kaiser-06/" target="_blank"><span><FontAwesomeIcon icon={faLinkedin} /></span></a>
-                     
+
                   </li>
                </ul>
             </div>
