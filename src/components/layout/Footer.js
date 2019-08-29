3b8656b82f81b72cab8fd26332ea5e0a96245242
Tomas Kaiser <tomas.kaiser06@gmail.com>
2019-08-29 21:41:49 +0100

    Removed unused variable

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { faDownload } from '@fortawesome/free-solid-svg-icons';
 
 function Footer() {
    return (
