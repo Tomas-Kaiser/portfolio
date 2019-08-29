914c4cea1c74409775db6990a48f2ec1c9e1ecb4
Tomas Kaiser <tomas.kaiser06@gmail.com>
2019-08-29 21:39:34 +0100

    Added margin to ul in hamburger menu

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,11 +3,11 @@ import React from 'react';
 function Navbar() {
    return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+         <button className="navbar-toggler ml-3" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
          </button>
          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav mx-auto">
+            <ul className="navbar-nav ml-3">
                <li className="nav-item">
                   <a className="nav-link current" href="#home">Home<span className="sr-only">(current)</span></a>
                </li>
