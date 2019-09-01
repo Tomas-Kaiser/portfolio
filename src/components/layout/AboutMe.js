65a46328d7c05f1389e085ffa862a2a617a593fc
Tomas Kaiser <tomas.kaiser06@gmail.com>
2019-09-01 20:03:07 +0100

    Typo

--- src/components/layout/AboutMe.js
+++ src/components/layout/AboutMe.js
@@ -5,7 +5,7 @@ function AboutMe() {
       <section id="about-me" className="container container-about fade">
          <article className="about-me">
             <h2>About Me</h2>
-            <p className="mb-0">I am an experienced full-stack software developer, holding a diploma in software development (<b>First Class Honours</b>), credit-rated by Edinburgh Napier University. I have experience with front-end (HTML5, CSS3, JS) and back-end (Python, SQL, noSQL) technologies, so as 5 years of work experience in B2B and B2C markets. Besides I am currently pursuing <b>Higher Diploma in Computer Science from National College of Irelend</b> (full time eveneing course)</p>
+            <p className="mb-0">I am an experienced full-stack software developer, holding a diploma in software development (<b>First Class Honours</b>), credit-rated by Edinburgh Napier University. I have experience with front-end (HTML5, CSS3, JS) and back-end (Python, SQL, noSQL) technologies, so as 5 years of work experience in B2B and B2C markets. Besides I am currently pursuing <b>Higher Diploma in Computer Science from National College of Ireland</b> (full time eveneing course)</p>
          </article>
       </section>
    )
